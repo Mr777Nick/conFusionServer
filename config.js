091895b443d6d2a1e236baa746037d6a2dc68b8b
Bagas Insani <bagas.insani@commsult.id>
2020-09-15 19:36:00 +0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '312481766499324',
+        clientSecret: '666f4ae4a8b5e4af0a540b57b629f3cc'
+    }
 }
